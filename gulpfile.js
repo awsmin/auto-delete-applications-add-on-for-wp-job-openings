45f56896704ab509aa59bc6d8e5ec12fcad6d19e
nithinjohn22 <nithin@awsm.in>
2024-02-06 16:02:42 +0530

    UPDATE: pot file

---
+++ gulpfile.js
@@ -0,0 +1,179 @@
+/**
+ * Automating Development Tasks
+ * ----------------------------
+ * @package wp-job-openings
+ * @since 1.0.0
+ */
+
+'use strict';
+
+/*============================= Dependencies =============================*/
+
+const gulp = require('gulp'),
+	config = require('./config'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	lineEC = require('gulp-line-ending-corrector'),
+	bs = require('browser-sync').create();
+
+/* --- Dependencies: css --- */
+const cleanCSS = require('gulp-clean-css'), // Minify CSS
+	autoprefixer = require('gulp-autoprefixer');
+
+/* --- Dependencies: js --- */
+const uglify = require('gulp-uglify'), // Minify JavaScript
+	stripDebug = require('gulp-strip-debug'); // Remove debugging stuffs
+
+/* --- Dependencies: i18n --- */
+const wpPot = require('gulp-wp-pot'),
+	sort = require('gulp-sort');
+
+/*================================= Tasks =================================*/
+
+let init = (cb) => {
+	console.log('-------------------------------------------');
+	console.log('<<<<<-------- WP Job Openings -------->>>>>');
+	console.log('-------------------------------------------');
+	cb();
+};
+
+/* --- Tasks: Browsersync --- */
+
+let browserSync = (cb) => {
+	bs.init({
+		ghostMode: false,
+		proxy: config.previewURL,
+		notify: false
+	});
+	cb();
+};
+let bsReload = (cb) => {
+	bs.reload();
+	cb();
+};
+browserSync.description = `Initialize Browsersync and proxy ${config.previewURL}`;
+gulp.task('browser-sync', browserSync);
+
+/* --- Tasks: CSS --- */
+
+for (let type in config.style) {
+	let styleTask = () => {
+		let src =
+			type === 'general'
+				? [ config.style.general.src + '*.css' ]
+				: [
+						config.style[type].src + 'vendors/*.css',
+						config.style[type].src + 'includes/*.css',
+						config.style[type].src + '*.css'
+					];
+		let outputName = config.style[type].outputName;
+		let dest = config.style[type].dest;
+
+		return gulp
+			.src(src, { sourcemaps: config.debug ? true : false })
+			.pipe(concat(outputName))
+			.pipe(autoprefixer())
+			.pipe(cleanCSS({ compatibility: 'ie9' }))
+			.pipe(rename({ suffix: '.min' }))
+			.pipe(lineEC())
+			.pipe(gulp.dest(dest, { sourcemaps: config.debug ? '.' : false }));
+	};
+	let loadStyleTask = () => {
+		let src = config.style[type].dest;
+		return gulp.src(src + '*.css').pipe(bs.stream());
+	};
+	styleTask.description = `Concatenate ${type} styles and minify it`;
+	gulp.task(`${type}-style`, styleTask);
+	gulp.task(`load-${type}-styles`, gulp.series(`${type}-style`, loadStyleTask));
+}
+
+/* --- Tasks: JS --- */
+
+for (let type in config.scripts) {
+	let scriptTask = () => {
+		let src = [ config.scripts[type].src + '*.js' ];
+		let outputName = config.scripts[type].outputName;
+		let dest = config.scripts[type].dest;
+		let stream = gulp.src(src, { sourcemaps: config.debug ? true : false });
+		if (!config.debug) {
+			stream = stream.pipe(stripDebug());
+		}
+		stream = stream
+			.pipe(concat(outputName))
+			.pipe(uglify())
+			.pipe(rename({ suffix: '.min' }))
+			.pipe(lineEC())
+			.pipe(gulp.dest(dest, { sourcemaps: config.debug ? '.' : false }));
+		return stream;
+	};
+	scriptTask.description = `Concatenate ${type} js files and minify it`;
+	gulp.task(`${type}-scripts`, scriptTask);
+	gulp.task(`load-${type}-scripts`, gulp.series(`${type}-scripts`, bsReload));
+}
+
+/* --- Tasks: i18n --- */
+
+let i18n = () => {
+	return gulp
+		.src([ './**/*.php', '!./build/**/*.php', '!./vendor/**/*.php' ])
+		.pipe(sort())
+		.pipe(
+			wpPot({
+				domain: config.translation.domain,
+				package: config.translation.package,
+				team: config.translation.team
+			})
+		)
+		.pipe(gulp.dest(config.translation.dest));
+};
+i18n.description = 'Generates pot file for plugin localization';
+gulp.task('translate', i18n);
+
+/* --- Tasks: Watch files for any change --- */
+
+let watchFiles = () => {
+	gulp.watch('./**/*.php', bsReload);
+	for (let type in config.style) {
+		gulp.watch(config.style[type].src + '**/*.css', gulp.series(`load-${type}-styles`));
+	}
+	for (let type in config.scripts) {
+		gulp.watch(config.scripts[type].src + '**/*.js', gulp.series(`load-${type}-scripts`));
+	}
+};
+watchFiles.description = 'Watch PHP, JS and CSS files for any change';
+gulp.task(
+	'watch',
+	gulp.series(
+		browserSync,
+		gulp.parallel(
+			'admin-style',
+			'admin-scripts'
+		),
+		watchFiles
+	)
+);
+
+/* --- Tasks: Default tasks --- */
+gulp.task(
+	'default',
+	gulp.series(
+		init,
+		gulp.parallel(
+			'admin-style',
+			'admin-scripts'
+		),
+		browserSync
+	)
+);
+
+/* --- Tasks: Build tasks --- */
+gulp.task(
+	'build',
+	gulp.series(
+		init,
+		gulp.parallel(
+			'admin-style',
+			'admin-scripts'
+		)
+	)
+);
